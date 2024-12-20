070692bb9285bf3ac30a693c2f5278ad6c63ef24
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2024-12-20 09:09:50 +0100

    Delete shop directory

--- shop/store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from store.models import Product, Order, Cart, Bank_account
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Cart)
-admin.site.register(Bank_account)
